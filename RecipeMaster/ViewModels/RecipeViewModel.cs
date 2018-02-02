0c8fe7a532fdb4fafece6dbe2efa8bfaf115d5c1
cvcolomb <craig@craigcad.us>
2018-02-02 10:34:58 -0800

    XAML TreeView and WIP
    Adding TreeView sample project

--- RecipeMaster/ViewModels/RecipeViewModel.cs
+++ RecipeMaster/ViewModels/RecipeViewModel.cs
@@ -9,12 +9,15 @@ using System.Windows.Input;
 
 using RecipeMaster.Models;
 using RecipeMaster.Services;
+using RecipeMaster.Views;
 
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Template10.Services;
 using Template10.Mvvm;
 using Template10.Services.NavigationService;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Navigation;
 
 namespace RecipeMaster.ViewModels
 {
@@ -27,42 +30,89 @@ namespace RecipeMaster.ViewModels
 
         public RecipeViewModel()
         {
-            
+			ingredients = new ObservableCollection<Ingredient>();
         }
 
-        //public async Task LoadDataAsync(VisualState currentState)
-        //{
-            //_currentState = currentState;
-            //SampleItems.Clear();
+		private Recipe currentRecipe;
+		public Recipe CurrentRecipe
 
-            //var data = await SampleDataService.GetSampleModelDataAsync();
+		{
+			get { return currentRecipe; }
+			set
+			{
+				Set(ref currentRecipe, value);
+				Ingredients = currentRecipe.Ingredients;
+			}
+		}
 
-            //foreach (var item in data)
-            //{
-            //    SampleItems.Add(item);
-            //}
-            //Selected = SampleItems.First();
-        //}
 
-        //private void OnStateChanged(VisualStateChangedEventArgs args)
-        //{
-        //    _currentState = args.NewState;
-        //}
+		private ObservableCollection<Ingredient> ingredients;
+		public ObservableCollection<Ingredient> Ingredients
+		{
+			get => currentRecipe.Ingredients;
+			set
+			{
+				Set(ref ingredients, value);
+				CurrentRecipe.Ingredients = ingredients;
+			}
+		}
 
-        //private void OnItemClick(ItemClickEventArgs args)
-        //{
-        //    Order item = args?.ClickedItem as Order;
-        //    if (item != null)
-        //    {
-        //        //if (_currentState.Name == NarrowStateName)
-        //        //{
-        //        //    NavigationService.Navigate(typeof(RecipesDetailViewModel).FullName, item);
-        //        //}
-        //        //else
-        //        //{
-        //        //    Selected = item;
-        //        //}
-        //    }
-        //}
-    }
+		private Ingredient selectedIngredient;
+		public Ingredient SelectedIngredient
+		{
+			get { return selectedIngredient; }
+			set { Set(ref selectedIngredient, value); }
+		}
+
+
+		//public async Task LoadDataAsync(VisualState currentState)
+		//{
+		//_currentState = currentState;
+		//SampleItems.Clear();
+
+		//var data = await SampleDataService.GetSampleModelDataAsync();
+
+		//foreach (var item in data)
+		//{
+		//    SampleItems.Add(item);
+		//}
+		//Selected = SampleItems.First();
+		//}
+
+		//private void OnStateChanged(VisualStateChangedEventArgs args)
+		//{
+		//    _currentState = args.NewState;
+		//}
+
+		//private void OnItemClick(ItemClickEventArgs args)
+		//{
+		//    Order item = args?.ClickedItem as Order;
+		//    if (item != null)
+		//    {
+		//        //if (_currentState.Name == NarrowStateName)
+		//        //{
+		//        //    NavigationService.Navigate(typeof(RecipesDetailViewModel).FullName, item);
+		//        //}
+		//        //else
+		//        //{
+		//        //    Selected = item;
+		//        //}
+		//    }
+		//}
+
+		public async Task NewIngredientAsync()
+		{
+			var dialog = new NewNamedItemDialog();
+			var result = await dialog.ShowAsync();
+
+			Ingredient newIngredient = new Ingredient(dialog.TextEntry, IngredientType.Complex, SelectedIn)
+		}
+
+		public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+		{
+			CurrentRecipe = parameter as Recipe;
+			RaisePropertyChanged();
+			return base.OnNavigatedToAsync(parameter, mode, state);
+		}
+	}
 }
